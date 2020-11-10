ff03148f9c54458088494edcc432d5bdf8e69499
jinkq <1154609706@qq.com>
2020-11-10 15:23:29 +0000

    try-seaborn

--- src/main/python/plot.py
+++ src/main/python/plot.py
@@ -1,6 +1,20 @@
 import matplotlib.pyplot as plt
-x = np.linspace(1, 10, 1000)
-y = x
+import seaborn as sns
+import pandas as pd
 
-plt.plot(x,y,'r',linewidth=2)
-plt.show()
+
+def read_file(path):
+    with open(path) as f:
+        data = f.readlines();
+    df = pd.DataFrame()
+    for row in data:
+        x = row.split(',')[0]
+        y = row.split(',')[1].split('\t')[0]
+        label = int(row.split('\t')[1].strip())
+        instance = pd.DataFrame({'x':x, 'y':y, 'label':label}, index=[0])
+        df = pd.concat([df, instance])
+    return df
+df = read_file('../../../output/clusteredInstances/part-m-00000')
+
+fig = sns.scatterplot(x="x", y="y", data=df, hue='label', style='label');
+plt.savefig('../../../result/result.png')
